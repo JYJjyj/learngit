275c9d12eb0309adcd5d052b858af51c765aefbb
“JYJjyj” <jyj291993531@qq.com>
2018-04-23 08:23:22 -0700

    DLinkList with head and tail

--- DLinkList.c
+++ DLinkList.c
@@ -1,113 +1,82 @@
-//带头结点的双向链表
-#include <stdio.h>
-#include <assert.h>
-#include <malloc.h>
-#include"DLinkList.h"
-//typedef struct DLinkNode { 
-//DLinkType data; 
-//struct DLinkNode* next; 
-//struct DLinkNode* prev; 
-//} DLinkNode; 
-//初始化一个双链表
-void DLinkListInit(DLinkNode** head)
-{
-    assert(head);
-    (*head) = (DLinkNode*)malloc(sizeof(DLinkNode));
-    (*head)->next = NULL;
-    (*head)->prev = NULL;
-}
-//创建一个结点
+#include "DLinkList.h"
+
+//新建一个结点
 DLinkNode* CreateNode(DLinkType value)
 {
-    DLinkNode* ret = (DLinkNode*)malloc(sizeof(DLinkNode));
+    DLinkNode *ret = (DLinkNode*)malloc(sizeof(DLinkNode));
     ret->data = value;
-    ret->next = NULL;
     ret->prev = NULL;
+    ret->next = NULL;
     return ret;
 }
 //销毁一个结点
-void DestroyNode(DLinkNode *ptr)
+void DestroyNode(DLinkNode *node)
 {
-    free(ptr);
-    ptr = NULL;
+    free(node);
+    node = NULL;
 }
-//双链表的尾插
-DLinkNode* DLinkListPushBack(DLinkNode* head, DLinkType value)
+void DLinkListInit(DLinkList** l)
 {
-    assert(head != NULL);
+    assert(l);
+    (*l) = (DLinkList*)malloc(sizeof(DLinkNode));
+    (*l)->head = CreateNode('x');
+    (*l)->tail = (*l)->head;
+}
+DLinkNode* DLinkListPushBack(DLinkList* l, DLinkType value)
+{
+    assert(l);
     DLinkNode *newnode = CreateNode(value);
-    newnode->next = NULL;
-    DLinkNode *end = head;
-    while(end->next != NULL)
-    {
-        end = end->next;
-    }
-    end->next = newnode;
-    newnode->prev = end;
-    return newnode;
+    l->tail->next = newnode;
+    newnode->prev = l->tail;
+    l->tail = newnode;
+    return l->tail;
 }
-// 尾删
-void DLinListPopBack(DLinkNode* head)
+
+void DLinListPopBack(DLinkList* l)
 {
-    assert(head);
-    DLinkNode *tmp = head;
-    if(tmp->next == NULL)
-        return;
-    if(tmp->next->next == NULL)
-    {
-        DestroyNode(tmp->next);
-        head->next = NULL;
-        return;
-    }
-    while(tmp->next->next)
-    {
-        tmp = tmp->next;
-    }
-    DestroyNode(tmp->next);
+    DLinkNode *tmp = l->tail->prev;
     tmp->next = NULL;
+    DestroyNode(l->tail);
+    l->tail = tmp;
 }
-//头插
-void DLinkListPushFront(DLinkNode* head, DLinkType value)
+
+//
+void DLinkListPushFront(DLinkList* l, DLinkType value)
 {
-    assert(head);
+    assert(l);
     DLinkNode *newnode = CreateNode(value);
-    if(head->next == NULL)
+    newnode->prev = l->head;
+    newnode->next =l->head->next;
+    if(l->head->next == NULL)
     {
-        head->next = newnode;
-        newnode->next = NULL;
-        newnode->prev = head;
+        l->head->next = newnode;
+        l->tail = newnode;
         return;
     }
-    newnode->next = head->next;
-    head->next->prev = newnode;
-    head->next = newnode;
-    newnode->prev = head;
+    l->head->next = newnode;
+    newnode->next->prev = newnode;
 }
-//头删
-void DLinkListPopFront(DLinkNode* head)
+void DLinkListPopFront(DLinkList* l)
 {
-    assert(head);
-    //只有一个头节点
-    if(head->next == NULL)
+    assert(l);
+    if(l->head == NULL)
+        return;
+    DLinkNode *tmp = l->head->next;
+    l->head->next = tmp->next;
+    DestroyNode(tmp);
+    if(l->head->next == NULL)
     {
+        l->tail = l->head;
         return;
     }
-    DLinkNode *to_delete = head->next;
-    head->next = to_delete->next;
-    DestroyNode(to_delete);
-    if(head->next!=NULL)
-    {
-        head->next->prev = head;
-    }
+    l->head->next->prev = l->head;
 }
-//在链表中查找
-DLinkNode* DLinkListFind(DLinkNode* head, DLinkType to_find)
+//
+DLinkNode* DLinkListFind(DLinkList* l, DLinkType to_find)
 {
-    assert(head);
-    if(head->next == NULL)
-        return NULL;
-    DLinkNode *tmp = head->next;
-    while(tmp)
+    assert(l);
+    DLinkNode *tmp = l->head->next;
+    while(tmp != NULL)
     {
         if(tmp->data == to_find)
             return tmp;
@@ -116,22 +85,18 @@ DLinkNode* DLinkListFind(DLinkNode* head, DLinkType to_find)
     return NULL;
 }
 //
-///** 
-//* @brief 往指定位置之前插入一个元素 
-//*/ 
+////往指定位置之前插入一个元素 
 void DLinkListInsert(DLinkNode* pos, DLinkType value)
 {
     assert(pos);
     DLinkNode *newnode = CreateNode(value);
     newnode->next = pos;
     newnode->prev = pos->prev;
-    pos->prev = newnode;
     newnode->prev->next = newnode;
+    pos->prev = newnode;
 }
 //
-///** 
-//* @brief 往指定位置之后插入一个元素 
-//*/ 
+////往指定位置之后插入一个元素 
 void DLinkListInsertAfter(DLinkNode* pos, DLinkType value)
 {
     assert(pos);
@@ -141,133 +106,139 @@ void DLinkListInsertAfter(DLinkNode* pos, DLinkType value)
     pos->next = newnode;
     newnode->next->prev = newnode;
 }
-void PrintList(DLinkNode *head)
+//
+void DLinkListErase(DLinkList *l)
 {
-    assert(head);
-    printf("[head] ");
-    DLinkNode *tmp = head->next;
-    if(tmp == NULL)
-        return;
+    assert(l);
+    DLinkNode *tmp = l->head;
     while(tmp != NULL)
     {
+        DLinkNode *next = tmp->next;
+        DestroyNode(tmp);
+        tmp = next;
+    }
+    l->tail = l->head;
+}
+size_t DLinkListSize(DLinkList *l)
+{
+    DLinkNode *tmp = l->head;
+    size_t count = 0;
+    while(tmp != l->tail->next)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+
+//参数非法返回 -1 ，空链表返回 1 ，非空链表返回0。
+int DLinkListEmpty(DLinkList *l)
+{
+    if(l == NULL)
+        return -1;
+    else if(l->head == NULL)
+        return -1;
+    else if(l->head->next == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+void Print(DLinkList *l,const char *msg)
+{
+    printf("%s:",msg);
+    DLinkNode *tmp = l->head->next;
+    printf("[head] ");
+    while(tmp!=l->tail->next)
+    {  
         printf("[%c] ",tmp->data);
         tmp = tmp->next;
     }
     printf("\n");
 }
-
-////////////////////////////测试代码/////////////
-#define TESTHEAD printf("------------------------------------------%s------------------------------------------\n",__FUNCTION__);
-
+///////////////////////////////////测试代码///////////////////////////
+#define TESTHEAD printf("-----------------------------%s--------------------------------\n",__FUNCTION__)
 void testPushBack()
 {
     TESTHEAD;
-    DLinkNode *head;
-    DLinkListInit(&head);
-    DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-    PrintList(head);
+    DLinkList *l;
+    DLinkListInit(&l);
+    DLinkListPushBack(l,'a');
+    DLinkListPushBack(l,'b');
+    DLinkListPushBack(l,'c');
+    DLinkListPushBack(l,'d');
+    DLinkListPushBack(l,'e');
+    DLinkListPushBack(l,'f');
+    Print(l,"尾插六个元素");
 }
 void testPopBack()
 {
     TESTHEAD;
-    DLinkNode *head;
-    DLinkListInit(&head);
-    DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-    printf("尾删前：");
-    PrintList(head);
-    DLinListPopBack(head);
-    printf("尾删后：");
-    PrintList(head);
+    DLinkList *l;
+    DLinkListInit(&l);
+    DLinkListPushBack(l,'a');
+    DLinkListPushBack(l,'b');
+    DLinkListPushBack(l,'c');
+    DLinkListPushBack(l,'d');
+    DLinkListPushBack(l,'e');
+    DLinkListPushBack(l,'f');
+    Print(l,"尾插后");
+    DLinListPopBack(l);
+    Print(l,"尾删一个元素后");
 }
 void testPushFront()
 {
     TESTHEAD;
-    DLinkNode *head;
-    DLinkListInit(&head);
-    DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-    printf("头插前：");
-    PrintList(head);
-    DLinkListPushFront(head,'1');
-    printf("头插后：");
-    PrintList(head);
+    DLinkList *l;
+    DLinkListInit(&l);
+    DLinkListPushFront(l,'a');
+    DLinkListPushFront(l,'b');
+    DLinkListPushFront(l,'c');
+    DLinkListPushFront(l,'d');
+    DLinkListPushFront(l,'e');
+    DLinkListPushFront(l,'f');
+    Print(l,"头插后");
+
 }
 void testPopFront()
 {
     TESTHEAD;
-    DLinkNode *head;
-    DLinkListInit(&head);
-    DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-    printf("头删前：");
-    PrintList(head);
-    DLinkListPopFront(head);
-    printf("头删后：");
-    PrintList(head);
-    printf("\n");
+    DLinkList *l;
+    DLinkListInit(&l);
+    DLinkListPushFront(l,'a');
+    DLinkListPushFront(l,'b');
+    DLinkListPushFront(l,'c');
+    DLinkListPushFront(l,'d');
+    DLinkListPushFront(l,'e');
+    DLinkListPushFront(l,'f');
+    Print(l,"头插后");
+    DLinkListPopFront(l);
+    DLinkListPopFront(l);
+    DLinkListPopFront(l);
+    DLinkListPopFront(l);
+    DLinkListPopFront(l);
+    DLinkListPopFront(l);
+    Print(l,"头删：");
 }
-void testFind()
+void testListFind()
 {
     TESTHEAD;
-    DLinkNode *head;
-    DLinkNode *ret;
-    DLinkListInit(&head);
-    DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-    PrintList(head);
-    ret = DLinkListFind(head,'f');
-    if(ret)
+    DLinkList *l;
+    DLinkListInit(&l);
+    DLinkListPushFront(l,'a');
+    DLinkListPushFront(l,'b');
+    DLinkListPushFront(l,'c');
+    DLinkListPushFront(l,'d');
+    DLinkListPushFront(l,'e');
+    DLinkListPushFront(l,'f');
+    Print(l,"头插后");
+    DLinkNode *ret = DLinkListFind(l,'x');
+    if(ret == NULL)
     {
-        printf("找到了！\n");
-    }
-    else
         printf("没找到！\n");
-}
-void testInsert()
-{
-    TESTHEAD;
-    DLinkNode *head;
-    DLinkNode *pos ;
-    DLinkListInit(&head);
-    pos = DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-
-    DLinkListInsert(pos,'x');
-    PrintList(head);
-}
-void testInsertAfter()
-{
-    TESTHEAD;
-    DLinkNode *head;
-    DLinkNode *pos;
-    DLinkListInit(&head);
-    pos = DLinkListPushBack(head,'a');
-    DLinkListPushBack(head,'b');
-    DLinkListPushBack(head,'c');
-    DLinkListPushBack(head,'d');
-    DLinkListPushBack(head,'e');
-    DLinkListInsertAfter(pos,'x');
-    PrintList(head);
+    }
+    else if(ret->data == 'x')
+        printf("找到了！\n");
 }
 void test()
 {
@@ -275,11 +246,8 @@ void test()
     testPopBack();
     testPushFront();
     testPopFront();
-    testFind();
-    testInsert();
-    testInsertAfter();
+    testListFind();
 }
-
 int main()
 {
     test();
