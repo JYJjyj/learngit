905ba79099e1ecdf21991acd62495c5474c10353
“JYJjyj” <jyj291993531@qq.com>
2018-04-24 01:56:42 -0700

    DLinkList with tail II

--- DLinkList.h
+++ DLinkList.h
@@ -1,32 +1,48 @@
-//c语言实现一个双链表
+//带有头指针与尾指针的双向链表(有头指针)
 #pragma once
-
+#include<stdio.h>
+#include<assert.h>
+#include<malloc.h>
+#include<stddef.h>
 typedef char DLinkType; 
 
 typedef struct DLinkNode { 
-DLinkType data; 
-struct DLinkNode* next; 
-struct DLinkNode* prev; 
+    DLinkType data; 
+    struct DLinkNode* next; 
+    struct DLinkNode* prev; 
 } DLinkNode; 
+typedef struct DLinkList
+{
+    //头指针
+    DLinkNode *head;
+    //尾指针
+    DLinkNode *tail;
+}DLinkList;
+//初始化链表
+void DLinkListInit(DLinkList** l); 
+//尾插一个结点
+DLinkNode* DLinkListPushBack(DLinkList* l, DLinkType value); 
+//尾删
+void DLinListPopBack(DLinkList* l); 
+//头插
+void DLinkListPushFront(DLinkList* l, DLinkType value); 
+//头删
+void DLinkListPopFront(DLinkList* l); 
+//查找
+DLinkNode* DLinkListFind(DLinkList* l, DLinkType to_find); 
 
-void DLinkListInit(DLinkNode** head); 
-
-DLinkNode* DLinkListPushBack(DLinkNode* head, DLinkType value); 
-
-void DLinListPopBack(DLinkNode* head); 
-
-void DLinkListPushFront(DLinkNode* head, DLinkType value); 
-
-void DLinkListPopFront(DLinkNode* head);
-
-DLinkNode* DLinkListFind(DLinkNode* head, DLinkType to_find); 
-
-/** 
-* @brief 往指定位置之前插入一个元素 
-*/ 
+//往指定位置之前插入一个元素 
 void DLinkListInsert(DLinkNode* pos, DLinkType value); 
 
-/** 
-* @brief 往指定位置之后插入一个元素 
-*/ 
-void DLinkListInsertAfter(DLinkNode* pos, DLinkType value);
+//往指定位置之后插入一个元素 
+void DLinkListInsertAfter(DLinkList *l,DLinkNode* pos, DLinkType value); 
+//清空一个链表
+void DLinkListErase(); 
+//删除一个指定结点
+void DLinkListRemove(DLinkList *l,DLinkType value); 
+//删除所有在这个链表中的值为给定值的结点
+void DLinkListRemoveAll(DLinkList *l,DLinkType value); 
+//求一个链表的长度
+size_t DLinkListSize(); 
+//判断一个链表是否为空
+int DLinkListEmpty();
